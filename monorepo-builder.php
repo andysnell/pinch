7c4f01d1189e99c76ff22629e0785f470f071a6f
Andy Snell <andy@phoneburner.com>
2025-08-07 23:11:58 -0500

    Fix Missing GitHub Token

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Merge\JsonSchema;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
 
 return static function (MBConfig $config): void {
     $config->packageDirectories([__DIR__ . '/packages']);
     $config->defaultBranch('main');
     $config->composerSectionOrder(JsonSchema::getProperties());
     $config->packageAliasFormat('<major>.<minor>.x-dev');
-    $config->workers([
-        SetCurrentMutualDependenciesReleaseWorker::class,
-    ]);
 };
